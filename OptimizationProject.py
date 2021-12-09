c8395db287ae1e51f044e16ad173564a6ec0caca
dbkoury <dbkoury@email.wm.edu>
2021-12-08 20:51:59 -0500

    Update OptimizationProject.py

--- OptimizationProject.py
+++ OptimizationProject.py
@@ -406,8 +406,8 @@ TW = []
 for i in Y:
     temp = []
     for j in P:
-        TotalWidget += 0*L1[i][j] + ((WTD/WPF)*OWC[i])*L2[i][j] + ((WTD/WPF)*OWC[i] + (PC[j] - (WTD/WPF))*DWC[i])*L3[i][j]
-        temp.append(0*L1[i][j] + ((WTD/WPF)*OWC[i])*L2[i][j] + ((WTD/WPF)*OWC[i] + (PC[j] - (WTD/WPF))*DWC[i])*L3[i][j])
+        TotalWidget += 0*L1[i][j] + ((WTD)*OWC[i])*L2[i][j] + ((PC[j] - (WTD/WPF))*WPF*DWC[i])*L3[i][j]
+        temp.append(0*L1[i][j] + ((WTD)*OWC[i])*L2[i][j] + ((PC[j] - (WTD/WPF))*WPF*DWC[i])*L3[i][j])
     TW.append(temp)
         
 #Sum the costs
@@ -420,7 +420,7 @@ print(f"status={LpStatus[status]}")
 print(f"Cost=${round(value(objective)*1000,2)}") 
 
 
-#create graph of supply chain nodes
+#Supply Chain Nodes
     
 Pcolors = ['lightblue','lightgreen','lightpink','yellow','thistle']     
 Wcolors= ['blue','green','red','purple']
@@ -514,6 +514,7 @@ ax.legend(loc="upper left", handles=DLegend,title="Retail Centers",prop={'size':
 ax.set_xlabel("Year")
 ax.set_ylabel("Demand (units)")
 plt.title("Projected Flugel Demand")
+plt.show
 
 
 #Plant Cost Charts 
@@ -549,11 +550,11 @@ for i in range(len(PValues)):
         plt.title(f"Projected/Realized {CTitles[i]} Costs\n${Total[-6:-3]},{Total[-3:]}")
     plt.show
     
-#Cost bar chart
+#Total Cost bar chart
 Costs =  [TAC,TPWSC ,TWRSC, TO, TC ,TR , TS , TW]
 label = ['Alloy Cost','P2W Shipping', 'W2R Shipping','Operations','Construction','Reopening Cost','Shutdown Cost','Widget Cost']
 fig,ax = plt.subplots()
-plt.ylim(0,12000)
+plt.ylim(0,22000)
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 colors = ['red','blue','orange','green','yellow','purple','brown','pink']
@@ -579,33 +580,214 @@ else:
     plt.title(f"Yearly Flugel Production Cost Strategy\n${Total[-6:-3]},{Total[-3:]}")
 ax.set_xlabel("Year")
 ax.set_ylabel("Cost (1000s of $)")
-ax.legend(loc=[.15,.72],title="Cost Type",prop={'size': 5})
+ax.legend(loc=[.05,.72],title="Cost Type",prop={'size': 5})
 plt.axhline(sum(YCost)/1000/len(YCost), color='k')
 plt.show
     
+#Plant to warehouse shipping numbers
+for i in Y:
+    fig,ax = plt.subplots()
+    plt.ylim(17,72)
+    plt.xlim(0,270)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.axes.xaxis.set_visible(False)
+    ax.axes.yaxis.set_visible(False)
+    plt.title("Plants: Flugel Production and Shipping Strategy", size=14,ha="center",weight='bold')
+    ax.text(10,65, s=f"Year {i+1}", size=9, ha="center", va="center", wrap=True, weight = 'bold')
+    plt.arrow(0,62,270,0, width=.1,color="black",head_width=None)
+    ax.text(40,65, s=f"Status", size=9, ha="center", va="center", wrap=True)
+    plt.arrow(26,68,0,-45, width=.1,color="black",head_width=None)
+    ax.text(140,70, s=f"Warehouse", size=11, ha="center", va="center", wrap=True, weight = 'bold')
+    ax.text(240,65, s=f"Total", size=9, ha="center", va="center", wrap=True)
+    plt.arrow(220,68,0,-45, width=.1,color="black",head_width=None)
+    
+    for j in W:
+        ax.text(80+(j*40),65, s=f"{j+1}", size=9, ha="center", va="center", wrap=True)
+        plt.arrow(58+(j*40),68,0,-45, width=.1,color="black",head_width=None)
+        Total = 0
+        for k in P:
+            Total += PWF[i][k][j].varValue
+        ax.text(80+(j*40),25, s=round(Total), size=9, ha="center", va="center", wrap=True)
+    for j in P:
+        ax.text(10,58-(j*7), s=f"Plant {j+1}", size=9, ha="center", va="center", wrap=True)
+        if CD[i][j].varValue == 1:
+            ax.text(40,58-(j*7), s=f"BUILD", size=9, ha="center", va="center", wrap=True)
+        elif O[i][j].varValue == 1:
+            ax.text(40,58-(j*7), s=f"OPEN", size=9, ha="center", va="center", wrap=True)
+        else:
+            ax.text(41,58-(j*7), s=f"CLOSED", size=9, ha="center", va="center", wrap=True)
+        plt.arrow(0,55-(j*7),270,0, width=.1,color="black",head_width=None)
+        ax.text(240,58-(j*7), s=round(value(lpSum(PWF[i][j]))), size=9, ha="center", va="center", wrap=True)
+        for k in W:
+            ax.text(80+(k*40),58-(j*7), s=round(PWF[i][j][k].varValue), size=9, ha="center", va="center", wrap=True)
+    ax.text(10,25, s=f"Total", size=9, ha="center", va="center", wrap=True)
+    ax.text(240,25, s=round(value(lpSum(PWF[i]))), size=9, ha="center", va="center", wrap=True)
+    ax.text(50,20, s=f"Total Shipping Cost:", size=12, ha="center", va="center", wrap=True, weight = 'bold')
+    Cost = str(round(value(lpSum(TPWSC[i]))*1000))
+    if len(Cost) > 6:
+        ax.text(110,20, s=f"${Cost[:-6]},{Cost[-6:-3]},{Cost[-3:]}", size=12, va="center", wrap=True)
+    else:
+        ax.text(110,20, s=f"${Cost[:-3]},{Cost[-3:]}", size=12, va="center", wrap=True)
+    plt.show
+    
+#Warehouse to retailer shipping numbers
+for i in Y:    
+    fig,ax = plt.subplots()
+    plt.ylim(-25,75)
+    plt.xlim(-10,270)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.axes.xaxis.set_visible(False)
+    ax.axes.yaxis.set_visible(False)
+    plt.title("Warehouses: Flugel Shipping Strategy", size=14,ha="center",weight='bold')
+    ax.text(25,65, s=f"Year {i+1}", size=9, ha="center", va="center", wrap=True,weight='bold')
+    plt.arrow(-10,62,280,0, width=.1,color="black",head_width=None)
+    
+    
+    ax.text(150,71, s=f"Warehouse", size=11, ha="center", va="center", wrap=True, weight='bold')
+    ax.text(250,65, s=f"Total", size=9, ha="center", va="center", wrap=True)
+    plt.arrow(230,68,0,-83, width=.1,color="black",head_width=None)
+    ax.text(28,58, s=f"Starting Inventory", size=9, ha="center", va="center", wrap=True)
+    plt.arrow(-10,55,280,0, width=.1,color="black",head_width=None)
+    ax.text(28,51, s=f"Incoming Inventory", size=9, ha="center", va="center", wrap=True)
+    plt.arrow(-10,48,280,0, width=.1,color="black",head_width=None)
+    for j in W:
+        ax.text(90+(j*40),65, s=f"{j+1}", size=9, ha="center", va="center", wrap=True)
+        plt.arrow(69+(j*40),68,0,-83, width=.1,color="black",head_width=None)
+        if i == 0:
+            ax.text(90+(j*40),58, s=f"0", size=9, ha="center", va="center", wrap=True)
+        else:
+            ax.text(90+(j*40),58, s=round(EI[i-1][j].varValue), size=9, ha="center", va="center", wrap=True)
+        ax.text(90+(j*40),-12, s=round(EI[i][j].varValue), size=9, ha="center", va="center", wrap=True)
+        Total = 0
+        for k in P:
+            Total += PWF[i][k][j].varValue
+        ax.text(90+(j*40),51, s=round(Total), size=9, ha="center", va="center", wrap=True)
+        for k in R:
+            ax.text(90+(j*40),44-(k*7), s=round(WRF[i][j][k].varValue), size=9, ha="center", va="center", wrap=True)
+    
+    for j in R:
+        ax.text(30,44-(j*7), s=f"Retail Center {j+1}", size=9, ha="center", va="center", wrap=True)
+        Total = 0
+        for k in W:
+            Total += WRF[i][k][j].varValue
+        ax.text(250,44-(j*7), s=round(Total), size=9, ha="center", va="center", wrap=True)
+        plt.arrow(-10,41-(j*7),280,0, width=.1,color="black",head_width=None)
+    ax.text(28,-12, s="Remaining Inventory", size=9, ha="center", va="center", wrap=True)
+    ax.text(50,-20, s=f"Total Shipping Cost:", size=12, ha="center", va="center", wrap=True, weight = 'bold')
+    Cost = str(round(value(lpSum(TWRSC[i]))*1000))
+    if len(Cost) > 6:
+        ax.text(110,-20, s=f"${Cost[:-6]},{Cost[-6:-3]},{Cost[-3:]}", size=12, va="center", wrap=True)
+    else:
+        ax.text(110,-20, s=f"${Cost[:-3]},{Cost[-3:]}", size=12, va="center", wrap=True)
+    plt.show
 
-#Output
-    #NOTE: I would recommend a breakdown of the yearly costs, production numbers, warehouse storage numbers, and shipping numbers using these tables:
-        #Production Costs/Plant 1/Plant 2/etc for each year or year 1/year 2/ etc. for each plant
-            #Number of Flugels
-            #Input Costs
-                #Widgets
-                #Alloy
-            #Opening Costs
-            #Operating Costs
-            #Reopening Costs
-            #ShutDown Costs
-            #Total Costs
+Used = []        
+for j in P:
+    Total = 0
+    for i in Y:
+        Total += CD[i][j].varValue
+    Used.append(Total)
+
+#Widget Cost bar chart    
+for i in range(len(Used)):
+    if Used[i] == 1:
+        label = ['Regular Pricee','Discount Price']
+        fig,ax = plt.subplots()
+        plt.ylim(0,7000)
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        colors = ['red','blue']
+        width = .8
+        ticks = []
+        WLegend = []
+        for j in range(len(colors)):
+            WLegend.append(pat.Patch(color=colors[j], label=label[j]))
+        RCosts = []
+        DCosts = []
+        for j in Y:
+            ticks.append(f"{j+1}")
+            if value(lpSum(PWF[j][i]))*WPF > WTD:
+                RCosts.append(WTD*OWC[j])
+                DCosts.append((value(lpSum(PWF[j][i]))*WPF-(WTD))*DWC[j])  
+            else:
+                RCosts.append(value(lpSum(PWF[j][i]))*WPF*OWC[j])
+                DCosts.append(0)
+        ax.bar(ticks, RCosts, width=width, bottom = [0,0,0,0,0,0,0,0,0,0], label=label[0],fc=colors[0])
+        ax.bar(ticks, DCosts, width=width, bottom = RCosts, label=label[1],fc=colors[1])
+
+        Total = str(round(sum(RCosts)+sum(DCosts)*1000))
+        if len(Total) > 6:
+            plt.title(f"Plant {i+1} Widget Order Costs\n${Total[:-6]},{Total[-6:-3]},{Total[-3:]}")
+        else:
+            plt.title(f"Plant {i+1} Widget Order Costs\n${Total[-6:-3]},{Total[-3:]}")
+        ax.set_xlabel("Year")
+        ax.set_ylabel("Cost (1000s of $)")
+        ax.legend(loc=[.05,.78],handles =WLegend, title="Cost Type",prop={'size': 8})
+        plt.show
+
+#Allot cost bar chart
+for i in range(len(Used)):
+    if Used[i] == 1:        
+        fig,ax = plt.subplots()
+        plt.ylim(0,2500)
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        width = .8
+        ticks = []
+
+        ACosts = []
+
+        for j in Y:
+            ticks.append(f"{j+1}")
+            ACosts.append(value(lpSum(PWF[j][i]))*APF*AC[j]) 
+        ax.bar(ticks, ACosts, width=width)
+
+        Total = str(round(sum(RCosts)*1000))
+        if len(Total) > 6:
+            plt.title(f"Plant {i+1} Alloy Order Costs\n${Total[:-6]},{Total[-6:-3]},{Total[-3:]}")
+        else:
+            plt.title(f"Plant {i+1} Alloy Order Costs\n${Total[-6:-3]},{Total[-3:]}")
+        ax.set_xlabel("Year")
+        ax.set_ylabel("Cost (1000s of $)")
+        plt.show
+
+#Flugel Production and resource order numbers
+for i in range(len(Used)):
+    if Used[i] == 1:        
+        fig,ax = plt.subplots()
+        plt.ylim(-7.5,75)
+        plt.xlim(15,230)
+        ax.spines['top'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.axes.xaxis.set_visible(False)
+        ax.axes.yaxis.set_visible(False)
+        plt.title(f"Plant {i + 1}: Production Strategy", size=14,ha="center",weight='bold')
+        ax.text(30,65, s=f"Year", size=9, ha="center", va="center", wrap=True,weight='bold')
+        plt.arrow(-10,62,280,0, width=.1,color="black",head_width=None)
+        
+        ax.text(170,71, s=f"Resources", size=11, ha="center", va="center", wrap=True, weight='bold')
+        plt.arrow(50,68,0,-83, width=.1,color="black",head_width=None)
+        ax.text(80,65, s=f"Flugels", size=9, ha="center", va="center", wrap=True,weight='bold')
+        plt.arrow(110,68,0,-83, width=.1,color="black",head_width=None)
+        ax.text(140,65, s=f"Alloy (lbs)", size=9, ha="center", va="center", wrap=True,weight='bold')
+        plt.arrow(170,68,0,-83, width=.1,color="black",head_width=None)
+        ax.text(200,65, s=f"Widgets", size=9, ha="center", va="center", wrap=True,weight='bold')
+
+        for j in Y:
+            ax.text(30,58-(j*7), s=f"{j+1}", size=9, ha="center", va="center", wrap=True)
+            plt.arrow(-10,55-(j*7),280,0, width=.1,color="black",head_width=None)
+            Total = 0
+            for k in W:
+                Total += PWF[j][i][k].varValue
+            ax.text(80,58-(j*7), s=round(Total), size=9, ha="center", va="center", wrap=True,weight='bold')
+            ax.text(140,58-(j*7), s=round(Total*APF), size=9, ha="center", va="center", wrap=True,weight='bold')
+            ax.text(200,58-(j*7), s=round(Total)*WPF, size=9, ha="center", va="center", wrap=True,weight='bold')
             
-#Schedule for plant operation and construction and shutdown and reopening
-#Bar graph showing percentages of costs per year 
-#tables with numbers of production, shipping, resource buying, storage numbers 
-        
-        #Shipping Costs and number shipped in a grid with plants and warehouses with a total bar grouped by plant
-        
-        #Shipping Costs and number shipped in a grid with warehouses and retailers with a total bar grouped by warehouse (tho maybe one on both axis)
-        
-    #An analysis of the results and perhaps some interesting conclusions and recommendations would be a good idea
-    #Given that the results are time series, some time series line graphs would also be interesting tracking costs
-
-
